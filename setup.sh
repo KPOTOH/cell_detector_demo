4d1e58c1b2066c38b646027e901f377d740582d3
KPOTOH <axepon@mail.ru>
2020-12-27 23:37:49 +0200

    final update with deprecations

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ echo "[general]
 email = \"your-email@domain.com\"
 " > ~/.streamlit/credentials.toml
 
-# echo "[server]
-# headless = true
-# enableCORS = false
-# port = $PORT
-# " > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+enableCORS = false
+port = $PORT
+" > ~/.streamlit/config.toml
